6851d6bcf9fda6d970cddcfbf2ff10dc7ca65b61
Nick Le Large <nick@le-large.com>
2023-08-12 00:21:23 +0200

    refactor: reorder functions

--- time_calculator.py
+++ time_calculator.py
@@ -8,49 +8,6 @@ weekdays = [
     "sunday"
 ]
 
-def add_12_hours(time_str):
-    return str(int(time_str.split(':')[0]) + 12) + ":" + time_str.split(':')[1]
-
-def to_24_hour_format(time_str):
-    if time_str.split()[1] == "PM":
-        time_str = add_12_hours(time_str)
-    return time_str.split()[0]
-
-def time_str_to_int(time_str):
-    hours, minutes = [int(str) for str in time_str.split(":")]
-    return hours * 60 + minutes
-
-def offset_str(offset_days):
-    if offset_days >= 2:
-        return "(" + str(offset_days) + " days later)"
-    elif offset_days >= 1:
-        return "(next day)"
-    return ""
-    
-def get_new_weekday(weekday, days_offset):
-    if weekday is None:
-        return ""
-    
-    weekday_number = weekdays.index(weekday.lower())
-    weekday_number += days_offset
-    weekday_number = weekday_number % 7
-    return weekdays[weekday_number].capitalize()
-
-def two_digit_string(input):
-    return ("0" + str(input))[-2:]
-
-def to_12_hour_format(time_str):
-    hour, minute = [int(str) for str in time_str.split(":")]
-
-    suffix = "AM"
-    if hour >= 12:
-        suffix = "PM"
-        hour -= 12
-    if hour == 0:
-        hour = 12
-
-    return str(hour) + ":" + two_digit_string(minute) + " " + suffix
-
 def add_time(start, duration, weekday = None):
     start_24h = to_24_hour_format(start)
     start_int = time_str_to_int(start_24h)
@@ -76,3 +33,47 @@ def add_time(start, duration, weekday = None):
                     offset_spacing + offset
     
     return new_time_str
+
+def to_24_hour_format(time_str):
+    if time_str.split()[1] == "PM":
+        time_str = add_12_hours(time_str)
+    return time_str.split()[0]
+
+def to_12_hour_format(time_str):
+    hour, minute = [int(str) for str in time_str.split(":")]
+
+    suffix = "AM"
+    if hour >= 12:
+        suffix = "PM"
+        hour -= 12
+    if hour == 0:
+        hour = 12
+
+    return str(hour) + ":" + two_digit_string(minute) + " " + suffix
+
+def add_12_hours(time_str):
+    return str(int(time_str.split(':')[0]) + 12) + ":" + time_str.split(':')[1]
+
+def time_str_to_int(time_str):
+    hours, minutes = [int(str) for str in time_str.split(":")]
+    return hours * 60 + minutes
+
+def offset_str(offset_days):
+    if offset_days >= 2:
+        return "(" + str(offset_days) + " days later)"
+    elif offset_days >= 1:
+        return "(next day)"
+    return ""
+    
+def get_new_weekday(weekday, days_offset):
+    if weekday is None:
+        return ""
+    
+    weekday_number = weekdays.index(weekday.lower())
+    weekday_number += days_offset
+    weekday_number = weekday_number % 7
+    return weekdays[weekday_number].capitalize()
+
+def two_digit_string(input):
+    return ("0" + str(input))[-2:]
+
